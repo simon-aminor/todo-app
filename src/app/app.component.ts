fa4b9b9120cb96b2d792a2a7c61669627985bafe
Saman <samanaminoroaya@gmail.com>
2024-10-29 11:43:53 +0330

    search box & delete + add button add fetch service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import { AppService } from './app.service';
+import { log } from 'console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule, MatDividerModule, MatIconModule,MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-app';
+  fetchService = inject(AppService)
+  typesOfShoes = []
+
+  ngOnInit(): void {
+   const data = this.fetchService.fetch().subscribe({
+    next:(res)=> {
+         console.log(res);
+         
+    },
+    error:(error)=> {
+        console.error = error
+    }
+})
+  
+  
+   
+    
+  }
+  
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { log } from 'console';
+import { resolve } from 'path';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppService {
+
+    http = inject(HttpClient)
+
+    fetch(){
+       return  this.http.get("https://jsonplaceholder.typicode.com/posts")
+    }
+}
